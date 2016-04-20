5cf65c9de3aaa0f0968a84298ddd8885b25b358b
David Sanchez <david.sanchez.falero@gmail.com>
2016-04-21 00:13:06 +0200

    environment config added

--- bookfinder/apis/lirarything_client.py
+++ bookfinder/apis/lirarything_client.py
@@ -1,10 +1,11 @@
 import os
+from bookfinder.environment import LIBRARYTHING_API_KEY
 
 
 class LibraryThingClient(object):
 
     def __init__(self):
-        self.api_key = os.environ.get('LIBRARYTHING_API_KEY')
+        self.api_key = LIBRARYTHING_API_KEY
         self.base_url = 'http://covers.librarything.com/devkey/{}/{}/isbn/{}'
 
     def get_cover_url(self, isbn, size='medium'):
